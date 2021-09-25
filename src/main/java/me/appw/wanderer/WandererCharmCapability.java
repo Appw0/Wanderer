b4b3e7ca29e77caec3fc04790076252008dbb45a
Appw0 <austinpaulpal@gmail.com>
2021-09-25 16:22:31 -0700

    Initial commit

---
+++ src/main/java/me/appw/wanderer/WandererCharmCapability.java
@@ -0,0 +1,9 @@
+package me.appw.wanderer;
+
+import net.minecraft.item.ItemStack;
+import top.theillusivec4.curios.api.type.capability.ICurio;
+
+public class WandererCharmCapability implements ICurio {
+    public WandererCharmCapability(ItemStack stack) {
+    }
+}
---
+++ src/main/java/me/appw/wanderer/WandererCharmItem.java
@@ -0,0 +1,33 @@
+package me.appw.wanderer;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.util.Direction;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.capabilities.ICapabilityProvider;
+import net.minecraftforge.common.util.LazyOptional;
+import top.theillusivec4.curios.api.CuriosCapability;
+import top.theillusivec4.curios.api.type.capability.ICurio;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+public class WandererCharmItem extends Item {
+    public WandererCharmItem() {
+        super(new Properties().maxStackSize(1).maxDamage(64).group(ItemGroup.TOOLS));
+    }
+
+    @Override
+    public ICapabilityProvider initCapabilities(ItemStack stack, CompoundNBT nbt) {
+        return new ICapabilityProvider() {
+            final LazyOptional<ICurio> capability = LazyOptional.of(() -> new WandererCharmCapability(stack));
+            @Nonnull
+            @Override
+            public <T> LazyOptional<T> getCapability(@Nonnull Capability<T> cap, @Nullable Direction side) {
+                return CuriosCapability.ITEM.orEmpty(cap, this.capability);
+            }
+        };
+    }
+}
