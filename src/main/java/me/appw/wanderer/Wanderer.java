09fe83ea8d4a3fefccb3bd929fbbc52077be81aa
Appw0 <austinpaulpal@gmail.com>
2021-09-25 22:55:50 -0700

    spaghetti code

--- src/main/java/me/appw/wanderer/Wanderer.java
+++ src/main/java/me/appw/wanderer/Wanderer.java
@@ -2,10 +2,19 @@ package me.appw.wanderer;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.*;
+import net.minecraft.util.math.BlockPos;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.event.entity.living.LivingDeathEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
@@ -13,22 +22,33 @@ import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
 import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import org.apache.commons.lang3.tuple.ImmutableTriple;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.jmx.Server;
+import top.theillusivec4.curios.api.CuriosApi;
 import top.theillusivec4.curios.api.SlotTypeMessage;
 
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Mod("wanderer")
 public class Wanderer {
 
-    private static final Logger LOGGER = LogManager.getLogger();
+    public static final String MODID = "wanderer";
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
+    public static final RegistryObject<Item> WANDERER_CHARM = ITEMS.register("wanderer_charm", WandererCharmItem::new);
+
+    public static final Logger LOGGER = LogManager.getLogger();
 
     public Wanderer() {
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
         MinecraftForge.EVENT_BUS.register(this);
+        ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
     }
 
     private void setup(final FMLCommonSetupEvent event) {
@@ -45,10 +65,51 @@ public class Wanderer {
     public void onServerStarting(FMLServerStartingEvent event) {
     }
 
-    @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.FORGE)
     public static class RegistryEvents {
         @SubscribeEvent
-        public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
+        public static void playerRespawn(PlayerEvent.PlayerRespawnEvent event) {
+            if (!event.getPlayer().level.isClientSide) {
+                ServerPlayerEntity player = (ServerPlayerEntity) event.getPlayer();
+                Optional<ImmutableTriple<String, Integer, ItemStack>> curioOpt = CuriosApi.getCuriosHelper().findEquippedCurio(WANDERER_CHARM.get(), player);
+                if (curioOpt.isPresent()) {
+                    ImmutableTriple<String, Integer, ItemStack> curio = curioOpt.get();
+                    CompoundNBT nbt = curio.right.getOrCreateTag();
+                    nbt.putBoolean("RecentDeath", true);
+
+                    ListNBT spawns = nbt.getList("StoredSpawns", 9);
+                    LOGGER.debug(spawns);
+                    if (spawns.size() > 0) {
+                        ListNBT NBTpos = spawns.getList(spawns.size() - 1);
+                        BlockPos pos = new BlockPos(NBTpos.getDouble(0), NBTpos.getDouble(1), NBTpos.getDouble(2));
+                        LOGGER.debug(pos);
+                        player.teleportTo(pos.getX(), pos.getY(), pos.getZ());
+                        curio.right.hurtAndBreak(1, player, p -> CuriosApi.getCuriosHelper().onBrokenCurio(curio.left, curio.middle, p));
+                    }
+                }
+            }
+        }
+        @SubscribeEvent
+        public static void playerDeath(LivingDeathEvent event) {
+            if (!event.getEntity().level.isClientSide) {
+                if (event.getEntity() instanceof ServerPlayerEntity) {
+                    ServerPlayerEntity player = (ServerPlayerEntity) event.getEntity();
+                    Optional<ImmutableTriple<String, Integer, ItemStack>> curio = CuriosApi.getCuriosHelper().findEquippedCurio(WANDERER_CHARM.get(), player);
+                    if (curio.isPresent()) {
+                        CompoundNBT nbt = curio.get().right.getOrCreateTag();
+                        ListNBT spawns = nbt.getList("StoredSpawns", 9);
+                        if (nbt.getBoolean("RecentDeath")) {
+                            spawns.remove(spawns.size() - 1);
+                        }
+
+//                        if (spawns.size() > 0) {
+//                            ListNBT NBTpos = spawns.getList(spawns.size() - 1);
+//                            BlockPos pos = new BlockPos(NBTpos.getDouble(0), NBTpos.getDouble(1), NBTpos.getDouble(2));
+//                            player.setRespawnPosition(player.level.dimension(), pos, player.getRandom().nextFloat(), true, false);
+//                        }
+                    }
+                }
+            }
         }
     }
 }
--- src/main/java/me/appw/wanderer/WandererCharmCapability.java
+++ src/main/java/me/appw/wanderer/WandererCharmCapability.java
@@ -1,9 +1,72 @@
 package me.appw.wanderer;
 
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.*;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.world.gen.Heightmap;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.common.capabilities.ICapabilitySerializable;
+import static net.minecraftforge.common.util.Constants.NBT.*;
+
+import net.minecraftforge.common.world.ForgeWorldType;
 import top.theillusivec4.curios.api.type.capability.ICurio;
 
+import javax.annotation.Nonnull;
+
 public class WandererCharmCapability implements ICurio {
+    ItemStack stack;
+
     public WandererCharmCapability(ItemStack stack) {
+        this.stack = stack;
+        CompoundNBT stackNBT = stack.getOrCreateTag();
+        if (!stackNBT.contains("StoredSpawns", TAG_LIST)) {
+            stackNBT.put("StoredSpawns", new ListNBT());
+        }
+    }
+
+    @Override
+    @Nonnull
+    public DropRule getDropRule(LivingEntity livingEntity) {
+        return DropRule.ALWAYS_KEEP;
+    }
+
+    @Override
+    public void curioTick(String identifier, int index, LivingEntity livingEntity) {
+        if (!livingEntity.level.isClientSide && livingEntity.tickCount % 1200 == 0) {
+            if (livingEntity instanceof ServerPlayerEntity) {
+                ServerPlayerEntity player = (ServerPlayerEntity) livingEntity;
+                CompoundNBT stackNBT = stack.getOrCreateTag();
+                ListNBT spawns = stackNBT.getList("StoredSpawns", 9);
+                Wanderer.LOGGER.debug(spawns);
+                ListNBT pos = spawns.getList(spawns.size() - 1);
+                BlockPos spawnPos = player.blockPosition();
+                double dist = spawnPos.distSqr(pos.getDouble(0), pos.getDouble(1), pos.getDouble(2), true);
+                if (dist > Math.pow(300, 2)) {
+                    ListNBT newPos = new ListNBT();
+                    while (!player.level.getBlockState(spawnPos.below()).isCollisionShapeFullBlock(player.level, spawnPos.below())
+                    && !player.level.isWaterAt(spawnPos.below())) {
+                        if (spawnPos.getY() == 1) {
+                            spawnPos = player.blockPosition();
+                        } else {
+                            spawnPos = spawnPos.below();
+                        }
+                    }
+                    newPos.add(DoubleNBT.valueOf(spawnPos.getX()));
+                    newPos.add(DoubleNBT.valueOf(spawnPos.getY()));
+                    newPos.add(DoubleNBT.valueOf(spawnPos.getZ()));
+                    newPos.add(StringNBT.valueOf(player.level.dimension().getRegistryName().toString()));
+                    spawns.add(newPos);
+                    stackNBT.putBoolean("RecentDeath", false);
+                    if (spawns.size() > 8) {
+                        spawns.remove(0);
+                    }
+                }
+            }
+        }
     }
 }
